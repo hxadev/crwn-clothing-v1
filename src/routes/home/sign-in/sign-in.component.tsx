d54e37467129550a7f4786bcdd127b9c14e36b95
Alfonso Hernández Xochipa <alfonso_h_x@hotmail.com>
2023-10-30 13:49:10 -0600

    Firebase integracion with redirection

--- src/routes/home/sign-in/sign-in.component.tsx
+++ src/routes/home/sign-in/sign-in.component.tsx
@@ -1,8 +1,27 @@
-import React from "react";
-import { createUserDocumentFromAuth, signInWithGooglePopup } from "../../../firebase/firebase.utils";
-
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import {
+  auth,
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+} from "../../../firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    async function getResult() {
+      const response = await redirectResult();
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+    getResult();
+  }, []);
+
+  const redirectResult = async () => {
+    const response = await getRedirectResult(auth);
+    return response;
+  };
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,8 +32,12 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirection
+      </button>
     </div>
   );
+  //Server side rendering
 };
 
 export default SignIn;
